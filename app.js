65203449aaa37f14e2a4d3aa54b395af9c74ac4d
Kuba Biel <jakubiel@o2.pl>
2022-01-01 21:48:38 +0100

    5

--- app.js
+++ app.js
@@ -1,37 +1,15 @@
-class Timer {
-	constructor(durationInput, startButton, pauseButton) {
-		this.durationInput = durationInput;
-		this.startButton = startButton;
-		this.pauseButton = pauseButton;
-
-		this.startButton.addEventListener("click", this.start);
-		this.pauseButton.addEventListener("click", this.pause);
-	}
-
-	start = () => {
-		this.tick();
-		this.interval = setInterval(this.tick, 1000);
-	};
-	pause = () => {
-		clearInterval(this.interval);
-	};
-
-	tick = () => {
-		const timeremaining = parseFloat(this.durationInput.value);
-		this.durationInput.value = timeremaining - 1;
-	};
-
-	getRemTime() {
-		return parseFloat(this.durationInput.value);
-	}
-
-	setTime(time) {
-		this.durationInput.value = time;
-	}
-}
-
 const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+	onStart() {
+		console.log("timer starts");
+	},
+	onTick() {
+		console.log("tick");
+	},
+	onComplete() {
+		console.log("complete");
+	},
+});
---
+++ timer.js
@@ -0,0 +1,50 @@
+class Timer {
+	constructor(durationInput, startButton, pauseButton, callbacks) {
+		this.durationInput = durationInput;
+		this.startButton = startButton;
+		this.pauseButton = pauseButton;
+		if (callbacks) {
+			this.onStart = callbacks.onStart;
+			this.onTick = callbacks.onTick;
+			this.onComplete = callbacks.onComplete;
+		}
+
+		this.startButton.addEventListener("click", this.start);
+		this.pauseButton.addEventListener("click", this.pause);
+	}
+
+	start = () => {
+		if (this.onStart) {
+			this.onStart();
+		}
+		this.tick();
+		this.interval = setInterval(this.tick, 1000);
+	};
+	//if start button clicked multiple times function runs multiple times
+
+	pause = () => {
+		clearInterval(this.interval);
+	};
+
+	tick = () => {
+		if (this.timeRemaining === 0) {
+			if (this.onComplete) {
+				this.onComplete();
+			}
+			this.pause();
+		} else {
+			if (this.onTick) {
+				this.onTick();
+			}
+			this.timeRemaining = this.timeRemaining - 1;
+		}
+	};
+
+	get timeRemaining() {
+		return parseFloat(this.durationInput.value);
+	}
+
+	set timeRemaining(time) {
+		this.durationInput.value = time;
+	}
+}
